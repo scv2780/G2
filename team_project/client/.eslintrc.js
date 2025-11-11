a2a2e76a30628ed73471a456b0fda70c09ff934f
song <islbvv@gmail.com>
2025-11-11 16:15:46 +0900

    11월 11일 템플릿 교체

--- team_project/client/.eslintrc.js
+++ team_project/client/.eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'off',
+  },
+};
--- team_project/client/vue.config.js
+++ team_project/client/vue.config.js
@@ -1,33 +1,11 @@
-const webpack = require('webpack');
-
 module.exports = {
-  configureWebpack: {
-    // Set up all the aliases we use in our app.
-    plugins: [
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 6,
-      }),
-    ],
-  },
-  pwa: {
-    name: 'Vue Argon Design',
-    themeColor: '#172b4d',
-    msTileColor: '#172b4d',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: '#172b4d',
-  },
   devServer: {
     proxy: {
       '/api': {
-        // 백엔드 서버 주소
         target: 'http://localhost:3000',
         changeOrigin: true,
         pathRewrite: { '^/api': '' },
       },
     },
-  },
-  css: {
-    // Enable CSS source maps.
-    sourceMap: process.env.NODE_ENV !== 'production',
   },
 };
